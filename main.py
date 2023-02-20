fc38b73c333999c2a963dfa0f2ca690f3fbfc0a1
Frankie <87681682+tomatofrankie@users.noreply.github.com>
2023-02-20 11:10:49 +0800

    Hide TOKEN

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("5267877519:AAFOXQdhT-moRnscpniJU608gywutyfjl_4", use_context=True)
+    updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     global dp
